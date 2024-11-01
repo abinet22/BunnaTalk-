3879647c2f6d65af1d6d64019b45699e62aab884
abinet22 <abinet22@gmail.com>
2024-11-01 16:17:10 +0300

    CI/CD configuration

---
+++ tests/_init_.py
@@ -0,0 +1,8 @@
+# tests/__init__.py
+
+"""
+This is the test package for my_flask_app.
+"""
+
+# Optionally import commonly used test utilities here
+from .test_example import *
---
+++ tests/test_example.py
@@ -0,0 +1,2 @@
+def test_example():
+    assert 1 == 1  # Simple test to verify the setup works
